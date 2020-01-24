538e7de982a317af890ded28fb91eca8bd564442
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-01-25 00:06:08 +0330

    ADD Redux Form To Login Page

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
+import {Field , reduxForm} from 'redux-form';
 
-const LoginForm : React.FC = () => {
+const ILoginForm : React.FC = (props : any) => {
+
+    const {handleSubmit} = props;
+
     return (
         <div className="card">
             <h2 className="text-center">Hi 👋! Welcome to 😻UsePay</h2>
             {/* <p className="text-center">You can access to all your donation details</p> */}
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
+                    <label>Email</label>
+                    <Field name="email" type="email"  component="input"/>
                 </div>
+                <div>
+                    <label>Password</label>
+                    <Field name="password" type="password" component="input" />
+                </div>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
 }
 
-export default LoginForm;
+const ILoginFormRedux = reduxForm({
+    form : 'LoginForm'
+})(ILoginForm)
+
+export default ILoginFormRedux;
